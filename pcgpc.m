346a7fd7ea166f43dba6c735b4a4a8a03c8b58b0
marcsous <17042435+marcsous@users.noreply.github.com>
2020-05-21 13:46:34 -0700

    Add files via upload

--- pcgpc.m
+++ pcgpc.m
@@ -1,26 +1,25 @@
 function [x,flag,relres,iter,resvec] = pcgpc(A,b,tol,maxit,M1,M2,x0)
-% Preconditioned conjugate gradient solver with modifications to
-% support the use of a penalty on Im(x), aka a phase constraint.
+% Preconditioned conjugate gradient (pcg) with modifications to
+% support the use of a penalty on Im(x) aka a phase constraint.
 %
 % Meant to be used with anonymous functions, A = @(x)myfunc(x),
 % where myfunc(x) should return:
-% - A*x           : minimize ||b-Ax||
-% - A*x+λ*x       : minimize ||b-Ax|| with penalty on ||x||
-% - A*x+i*λ*Im(x) : minimize ||b-Ax|| with penalty on ||Im(x)||
+% - A*x           : to minimize ||b-Ax||^2
+% - A*x+λ*x       : to minimize ||b-Ax||^2 + λ^2||x||^2
+% - A*x+λ*i*Im(x) : to minimize ||b-Ax||^2 + λ^2||Im(x)||^2
 %
 % References:
-% - Partial Fourier Partially Parallel Imaging
-%   Mark Bydder and Matthew D. Robson (MRM 2005;53:1393)
 % - An Introduction to the CG Method Without the Agonizing Pain
-%   Jonathan Richard Shewchuk (1994)
+%    Jonathan Richard Shewchuk (1994)
+% - Partial Fourier Partially Parallel Imaging
+%    Mark Bydder and Matthew D. Robson (MRM 2005;53:1393)
 %
-% Key modifications:
-% - use the real part only of the dot products
-% - clear variables to minimize memory usage (GPU)
-% - make return args compatible with matlab's pcg
-% - (experimental) set tol=0 to terminate early (Bazan, 2014)
+% Modifications:
+% - uses the real part only of the dot products
+% - allows multiple RHS vectors (b = [b1 b2 ...])
+% - mostly compatible with pcg (except M2 and flag)
 %
-% Usage: see Matlab's pcg function (except M2 is not supported)
+% Usage: see Matlab's pcg function (help pcg)
 
 % check arguments
 if nargin<2; error('Not enough input arguments.'); end
@@ -28,44 +27,54 @@ if ~exist('tol') || isempty(tol); tol = 1e-6; end
 if ~exist('maxit') || isempty(maxit); maxit = 20; end
 if ~exist('M1') || isempty(M1); M1 = @(arg) arg; end
 if exist('M2') && ~isempty(M2); error('M2 argument not supported'); end
-if isnumeric(A); A = @(arg) A * arg; end
-if isnumeric(M1); M1 = @(arg) M1 \ arg; end
-if ~iscolumn(b); error('Right hand side must be a column vector.'); end
+if ~ismatrix(b); error('b argument must be a column vector or 2d array'); end
 validateattributes(tol,{'numeric'},{'scalar','nonnegative','finite'},'','tol');
 validateattributes(maxit,{'numeric'},{'scalar','nonnegative','integer'},'','maxit');
+
+% not intended for matrix inputs but they can be supported
+if isnumeric(A); A = @(arg) A * arg; end
+if isnumeric(M1); M1 = @(arg) M1 \ arg; end
 
 % initialize
 t = tic;
 iter = 1;
 flag = 1;
-imin = Inf;
 if ~exist('x0') || isempty(x0)
     r = b;
     x = zeros(size(b),'like',b);
 else
     if ~isequal(size(x0),size(b))
-        error('x0 must be a column vector of length %i to match the problem size.',numel(b));
+        error('x0 must have length %i to match the problem size.',size(b,1));
     end
     x = x0;
     r = b - A(x);
 end
 d = M1(r);
-delta0 = norm(b);
-delta_new = real(r'*d);
-resvec(iter) = norm(r);
-solvec(iter) = norm(x);
+delta0 = vecnorm(b);
+delta_new = real(dot(r,d));
+resvec(iter,:) = vecnorm(r);
+solvec(iter,:) = vecnorm(x);
+
+% min norm solution
+xmin = x;
+imin = zeros(1,size(b,2));
 
 % main loop
-while maxit>0
+while maxit
     
     iter = iter+1;
 	clear q; q = A(d);
-	alpha = delta_new./real(d'*q);
+    if ~isequal(size(b),size(q))
+        error('b is [%s] but matrix vector product is [%s].',num2str(size(b)),num2str(size(q)));
+    end
+	alpha = delta_new./real(dot(d,q));
     
     % unsuccessful termination
-    if ~isfinite(alpha); flag = 4; break; end
+    if ~all(isfinite(alpha)); flag = 4; break; end
     
 	x = x + alpha.*d;
+    
+    % recalculate residual occasionally
     if mod(iter,50)==0
         r = b - A(x);
     else
@@ -73,59 +82,51 @@ while maxit>0
     end
 
     % residual and solution vectors
-    resvec(iter) = norm(r);
-    solvec(iter) = norm(x);
+    resvec(iter,:) = vecnorm(r);
+    solvec(iter,:) = vecnorm(x);
 
-    % retain best solution
-    if resvec(iter) < min(resvec(1:iter-1))
-        xmin = x;
-        imin = iter;
+    % keep best solution
+    ok = resvec(iter,:) < min(resvec(1:iter-1,:));
+    if any(ok)
+        xmin(:,ok) = x(:,ok);
+        imin(ok) = iter;
     end
     
     % successful termination
-    if resvec(iter)<tol*delta0; flag = 0; break; end
-
-    % early termination (Bazan et al, Linear Algebra Appl. 2014;21:316)
-    psi(iter) = resvec(iter).*solvec(iter);
-    if tol==0 && iter>3 && psi(iter) > psi(iter-1); flag = 5; break; end
+    if all(resvec(iter,:)<tol*delta0); flag = 0; break; end
 
     % unsuccessful termination
     if iter>maxit; flag = 1; break; end
 
     clear q; q = M1(r);
 	delta_old = delta_new;
-    delta_new = real(r'*q);
+    delta_new = real(dot(r,q));
    
     % unsuccessful termination
-    if delta_new<=0; flag = 4; break; end
+    if all(delta_new<=0); flag = 4; break; end
 
     beta = delta_new./delta_old;
     d = q + beta.*d;
 
 end
 
-% return best solution (based on resvec)
-if imin<iter
+% min norm solution
+ok = imin==iter;
+if any(~ok)
     flag = 3;
-    x = xmin;
-    iter = imin;
-else
-    iter = iter-1;
+    x(:,~ok) = xmin(:,~ok);
 end
 
-% return arguments
+% remeasure final residual
 if nargout>2
-    
-    % remeasure final residual
-    resvec(end) = norm(b-A(x));
-    
-    % matlab compatiblity
-    resvec = reshape(resvec,[],1);
-
+    resvec(end,:) = vecnorm(b-A(x));
 end
-relres = resvec(end)./delta0;
+relres = resvec(end,:)./delta0;
 
 % only display if flag not supplied
 if nargout<2
-    fprintf('pcg terminated at iteration %i (flag %i): relres = %e. ',iter,flag,relres); toc(t);
+    for k = 1:size(b,2)
+        fprintf('pcg terminated at iteration %i (flag %i): relres = %e.\n',imin(k),flag,relres(k)); 
+    end
+    toc(t);
 end
