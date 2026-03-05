6df7f6000c26209c26ab52d6964d9bb7bdaf9cd5
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2026-03-05 13:35:04 -0800

    Add files via upload

--- ti2t1.m
+++ ti2t1.m
@@ -3,6 +3,7 @@ function t1 = ti2t1(ti,tr)
 % return t1-to-null from ti at specified tr
 %
 
+% arg checks
 if ndims(ti)~=2 || ndims(tr)~=2
     error('ti and tr must be scalar, vector or matrix');
 end
@@ -12,8 +13,8 @@ t1 = zeros(size(ti.*tr));
 ti = repmat(ti,size(t1)./size(ti));
 tr = repmat(tr,size(t1)./size(tr));
 
-% find the t1 with the given nulltime at a given tr
-f = @(t1,ti,tr) t1.*(log(2)-log(1+exp(-tr./t1)))-ti;
+% find the t1 with the given nulltime at the given tr
+f = @(t1,ti,tr) t1.*(log(2)-log(1+exp(-tr./t1))) - ti;
 
 % loop over dimensions
 for k = 1:numel(t1)
