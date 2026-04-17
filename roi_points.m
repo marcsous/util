c1a7751f31fc6e9d3095eca1d44828df107f68d0
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2026-04-17 10:57:21 -0700

    Add files via upload

--- roi_points.m
+++ roi_points.m
@@ -7,9 +7,14 @@ function [x y s v] = roi_points(im,radius)
 % -radius is the no. pixels for ROIs [5]
 %
 % Controls:
-% Left click to drop an ROI. Right click to quit. Mouse wheel to
-% scroll slices. Esc to quit. Q to quit. X to delete. B to brighten.
-% D to darken.
+% -Left click to drop an ROI
+% -Right click to quit
+% -Mouse wheel to scroll slices
+% -Esc to quit
+% -Q to quit
+% -X to delete last ROI
+% -B to brighten
+% -D to darken
 
 [nx ny ns ne] = size(im);
 
@@ -22,31 +27,59 @@ elseif ~isscalar(radius)
     error('radius must be a scalar');
 end
 
-%% image for display of ROIs
-imd = double(im);
-
-figure(gcf); clf reset; subplot(1,1,1);
-
-%% capture user events
+%% initialize
 
 button = 0;
 slice = ceil(ns/2);
-x = []; y = []; s = []; old = [];
+range = -radius:radius;
+x = []; y = []; s = []; v = [];
+
+% circular mask
+mask = false(numel(range));
+for j = range
+    for k = range
+        if hypot(j,k)<=radius
+            mask(1+radius+j,1+radius+k) = true;
+        end
+    end
+end
+
+% reset figure
+figure(gcf); clf reset; subplot(1,1,1);
+
+% capture stray keyboard events that otherwise go to the commandline
+set(gcf,'WindowKeyPressFcn',@(src, event) disp(''));
+
+%% capture user events
 
 % quit with right click, Esc, q or Q
 while ~ismember(button,[3 27 81 113])
 
-    % display current slice
-    figure(gcf); imagesc(imd(:,:,slice));
+    % update image
+    figure(gcf); imagesc(im(:,:,slice));
     title('left click to select, right click to quit',slice);
 
-    % capture stray keyboard events that otherwise go to the commandline
-    set(gcf,'WindowKeyPressFcn',@(src, event) disp(''));
-    
+    % draw ROIs on the image
+    for n = 1:numel(x)
+        if slice==s(n)
+
+            % mean value inside mask
+            tmp = im(x(n)+range,y(n)+range,s);
+            v(n) = mean(tmp(mask));
+
+            % draw ROI on the image
+            text(y(n),x(n),'◯','Fontsize',2*radius+1,'HorizontalAlignment','center','Color','r');
+
+            if     abs(v(n))>10; fmt = '%.0f';
+            elseif abs(v(n))> 1; fmt = '%.1f';
+            else fmt = '%.2f'; end
+            text(y(n)+radius,x(n)+1,num2str(v(n),fmt),'Color','r','FontSize',14);
+
+        end
+    end
+
     % wait for user input
-    drawnow;
     [myy myx button] = ginputc(1);
-    drawnow;
 
     % handle user input
     switch button
@@ -57,10 +90,10 @@ while ~ismember(button,[3 27 81 113])
                 s(end+1) = slice;
 
         case 4; % roll wheel up
-                slice = min(slice+1,ns);
+                slice = max(slice-1,1);
 
         case 5; % roll wheel down
-                slice = max(slice-1,1);
+                slice = min(slice+1,ns);
     
         case {66,98} % brighten (b or B)
                 brighten(+0.1);
@@ -68,55 +101,13 @@ while ~ismember(button,[3 27 81 113])
         case {68,100} % darken (d or D)
                 brighten(-0.1);
 
-    end
-
-    % draw or delete ROI on the image
-    for j = -radius:radius
-        for k = -radius:radius
-            if hypot(j,k)<radius+0.5 && hypot(j,k)>radius-0.5
-                switch button
-                    case 1; % left click
-                        old(end+1) = imd(x(end)+j,y(end)+k,slice);
-                        imd(x(end)+j,y(end)+k,s(end)) = NaN;
-                    case {88,120}; % x or X
-                        if numel(old)>0
-                            imd(x(end)-j,y(end)-k,s(end)) = old(end);
-                            old(end) = [];
-                        end
+        case {88,120} % delete point (x or X)
+                if numel(x) > 0
+                    x = x(1:end-1);
+                    y = y(1:end-1);
+                    s = s(1:end-1);
+                    v = v(1:end-1);
                 end
-            end
         end
-    end
-
-    % now can delete point
-    if button==88 || button==120 % x or X
-        x = x(1:end-1);
-        y = y(1:end-1);
-        s = s(1:end-1);
-    end
-
-end
-
-%% extract rois
-
-v = [];
-vs = [];
-range = -radius:radius;
-
-mask = false(2*radius+1,2*radius+1);
-for j = -radius:radius
-    for k = -radius:radius
-        if hypot(j,k)<=radius
-            mask(1+radius+j,1+radius+k) = true;
-        end
-    end
-end
-
-for n = 1:numel(x)
-
-    tmp = double(im(x(n)+range,y(n)+range,s(n)));
-
-    v(n) = mean(tmp(mask));
-    vs(n) = std(tmp(mask));
 
 end
